12c47280d9598d12dc6010cf47530cf16b0ebf4e
Leehsm <shahzleehasimwork@gmail.com>
2023-06-15 17:27:10 +0800

    create and edit controller

--- app/Http/Controllers/Backend/ContactController.php
+++ app/Http/Controllers/Backend/ContactController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function View(){
+        return view('backend.contact.view');
+    }
+
+    public function Add(){
+        return view('backend.contact.add');
+    }
+
+    public function Edit(){
+        return view('backend.contact.edit');
+    }
 }
