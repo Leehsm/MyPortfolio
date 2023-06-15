1f11550727430fd6717148d3fff7ac29dc4ac35e
shahzlee <shahzleehasim@gmail.com>
2023-06-15 22:31:10 +0800

    add strore function for about

--- app/Http/Controllers/Backend/AboutController.php
+++ app/Http/Controllers/Backend/AboutController.php
@@ -5,14 +5,42 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\About;
+use Carbon\Carbon;
+
 class AboutController extends Controller
 {
     public function View(){
-        return view('backend.about.view');
+        $abouts = About::get();
+        // dd($abouts);
+        return view('backend.about.view', compact('abouts'));
     }
 
     public function Add(){
         return view('backend.about.add');
+    }
+
+    public function Store(Request $request){
+        About::insert([
+            'description' => $request->desc,
+            'current_title' => $request->jobtitle,
+            'detail' => $request->jobtitledesc,
+            'birthdate' => $request->birthdate,
+            'age' => $request->age,
+            'education' => $request->edu,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'address' => $request->address,
+            'website' => $request->web,
+            'created_at' =>  Carbon::now()
+    	]);
+
+	    $notification = array(
+			'message' => 'About Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('about-view')->with($notification);
     }
 
     public function Edit(){
