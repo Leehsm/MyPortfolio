0877aa59a55d06edaf2c7a964847724953fb0eeb
Leehsm <shahzleehasimwork@gmail.com>
2023-06-16 17:27:44 +0800

    create crud function for all resume

--- app/Http/Controllers/Backend/ResumeController.php
+++ app/Http/Controllers/Backend/ResumeController.php
@@ -25,6 +25,20 @@ class ResumeController extends Controller
         return view('backend.resume.desc.add');
     }
 
+    public function DescStore(Request $request){
+        Resume::insert([
+            'description' => $request->desc,
+            'created_at' =>  Carbon::now()
+    	]);
+
+	    $notification = array(
+			'message' => 'Description Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('resume-view')->with($notification);
+    }
+
     public function DescEdit(){
         return view('backend.resume.desc.edit');
     }
@@ -36,6 +50,24 @@ class ResumeController extends Controller
         return view('backend.resume.summary.add');
     }
 
+    public function SummaryStore(Request $request){
+        ResumeSummary::insert([
+            'name' => $request->name,
+            'summary' => $request->summary,
+            'phone' => $request->phone,
+            'alamat' => $request->address,
+            'email' => $request->email,
+            'created_at' =>  Carbon::now()
+    	]);
+
+	    $notification = array(
+			'message' => 'Summary Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('resume-view')->with($notification);
+    }
+
     public function SummaryEdit(){
         return view('backend.resume.summary.edit');
     }
@@ -45,6 +77,23 @@ class ResumeController extends Controller
         return view('backend.resume.edu.add');
     }
 
+    public function EduStore(Request $request){
+        ResumeEdu::insert([
+            'education' => $request->cert,
+            'year' => $request->year,
+            'university' => $request->uni,
+            'detail' => $request->detail,
+            'created_at' =>  Carbon::now()
+    	]);
+
+	    $notification = array(
+			'message' => 'Education Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('resume-view')->with($notification);
+    }
+
     public function EduEdit(){
         return view('backend.resume.edu.edit');
     }
@@ -52,6 +101,30 @@ class ResumeController extends Controller
 
     public function ExpAdd(){
         return view('backend.resume.exp.add');
+    }
+
+    public function ExpStore(Request $request){
+        ResumeExperience::insert([
+            'name' => $request->exp,
+            'year' => $request->year,
+            'company' => $request->company,
+            'detail1' => $request->detail1,
+            'detail2' => $request->detail2,
+            'detail3' => $request->detail3,
+            'detail4' => $request->detail4,
+            'detail5' => $request->detail5,
+            'detail6' => $request->detail6,
+            'detail7' => $request->detail7,
+            'detail8' => $request->detail8,
+            'created_at' =>  Carbon::now()
+    	]);
+
+	    $notification = array(
+			'message' => 'Summary Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->route('resume-view')->with($notification);
     }
 
     public function ExpEdit(){
