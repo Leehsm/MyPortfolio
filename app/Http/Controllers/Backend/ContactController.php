fe7812e52bfe2139442518ce0b7c021a8eb3d35d
Leehsm <shahzleehasimwork@gmail.com>
2023-06-16 16:02:47 +0800

    create store function

--- app/Http/Controllers/Backend/ContactController.php
+++ app/Http/Controllers/Backend/ContactController.php
@@ -4,15 +4,31 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use Carbon\Carbon;
 
 class ContactController extends Controller
 {
     public function View(){
-        return view('backend.contact.view');
+        $contacts = Contact::get();
+        return view('backend.contact.view', compact('contacts'));
     }
 
     public function Add(){
         return view('backend.contact.add');
+    }
+
+    public function Store(Request $request){
+        Contact::insert([
+            'description' => $request->desc,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address'=> $request->address,
+            'location' => $request->location,
+            'created_at' => Carbon::now()
+        ]);
+
+        return redirect()->route('contact-view')->with($notification);
     }
 
     public function Edit(){
