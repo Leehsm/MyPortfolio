6d3ac2a3ad0d3f9384e1b7602aa3e518e400145a
Leehsm <shahzleehasimwork@gmail.com>
2023-06-16 17:29:30 +0800

    create blade for portfolio part

--- resources/views/backend/portfolio/add.blade.php
+++ resources/views/backend/portfolio/add.blade.php
@@ -1,6 +1,23 @@
 @extends('backend.welcome')
 @section('content') 
 
+<section id="about" class="about">
+    <div class="container">
+        <div class="section-title">
+            <h2>Resume</h2>
+            <form method="post" action="{{ route('portfolio-add') }}" >
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Description</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="desc" >
+                </div>
 
+                <div class="text-xs-right">
+                    <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Submit">					 
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
 
 @endsection
--- resources/views/backend/portfolio/view.blade.php
+++ resources/views/backend/portfolio/view.blade.php
@@ -1,6 +1,56 @@
 @extends('backend.welcome')
 @section('content') 
 
+<section id="about" class="about">
+    <div class="container">
+        <div class="section-title">
+            <h2>Portfolio</h2>
+            <a href="{{ route('portfolio-add') }}" class="btn btn-primary me-md-2" type="button">ADD</a>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Description</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {{-- @foreach ($desc as $item) --}}
+                  <tr>
+                    <td>
+                      <a>Descripiton</a>
+                    </td>
+                    <td>       
+                        <a href="{{ route('portfolio-edit') }}"class="btn btn-primary" type="button">EDIT</a>
+                    </td>
+                  </tr>
+                  {{-- @endforeach --}}
+                </tbody>
+            </table>
 
+            <h2>List Of Project</h2>
+            <a href="{{ route('portfolio-projadd') }}" class="btn btn-primary me-md-2" type="button">ADD</a>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {{-- @foreach ( $summary as $item ) --}}
+                  <tr>
+                    <td>Image</td>
+                    <td>detail</td>
+                    <td>       
+                        <a href="{{ route('portfolio-projedit') }}" class="btn btn-primary me-md-2" type="button">EDIT</a>
+                    </td>
+                  </tr>
+                  {{-- @endforeach --}}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
 
 @endsection
