46e11ad0a39e4ba688e2c42bde44d3b3adda54e5
Leehsm <shahzleehasimwork@gmail.com>
2023-06-16 17:28:42 +0800

    create new blade for add and edit for portfolio proj

---
+++ resources/views/backend/portfolio/proj/add.blade.php
@@ -0,0 +1,28 @@
+@extends('backend.welcome')
+@section('content') 
+
+<section id="about" class="about">
+    <div class="container">
+        <div class="section-title">
+            <h2>Project</h2>
+            <form method="post" action="{{ route('portfolio-projadd') }}" >
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Image</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="desc" >
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Description of Project</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="desc" >
+                </div>
+
+                <div class="text-xs-right">
+                    <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Submit">					 
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+@endsection
