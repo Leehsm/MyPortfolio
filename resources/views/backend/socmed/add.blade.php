7f69be305f69e3fb1c46002bad1cd2150208bbad
Leehsm <shahzleehasimwork@gmail.com>
2023-06-21 11:01:14 +0800

    create add edit view blade file for socmed

--- resources/views/backend/socmed/add.blade.php
+++ resources/views/backend/socmed/add.blade.php
@@ -1,6 +1,41 @@
 @extends('backend.welcome')
-@section('content') 
+@section('content')
+
+<!--  About Section  -->
+<section id="about" class="about">
+    <div class="container">
 
 
+        <div class="section-title">
+            <h2>Add Contact</h2>
+            <form method="post" action="{{ route('socmed-store') }}" >
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Twitter</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="twitter" >
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Facebook</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="facebook">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Instagram</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="instagram">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Linkedin</label>
+                    <input type="text" class="form-control" id="exampleFormControlinput type=" name="linkedin">
+                </div>
+
+                <div class="text-xs-right">
+                    <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Submit">					 
+                </div>
+            </form>
+        </div>
+    </div>
+</section><!-- End About Section -->
 
 @endsection
--- resources/views/backend/socmed/view.blade.php
+++ resources/views/backend/socmed/view.blade.php
@@ -1,6 +1,38 @@
 @extends('backend.welcome')
 @section('content') 
 
-
+<!--  About Section  -->
+<section id="about" class="about">
+    <div class="container">
+        <div class="section-title">
+            <h2>Contact</h2>
+            <a href="{{ route('socmed-add') }}" class="btn btn-primary me-md-2" type="button">ADD</a>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Twitter</th>
+                    <th>Facebook</th>
+                    <th>Instagram</th>
+                    <th>Linkedin</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ( $socmed as $item )
+                  <tr>
+                    <td>{{ $item->twitter }}</td>
+                    <td>{{ $item->facebook }}</td>
+                    <td>{{ $item->instagram }}</td>
+                    <td>{{ $item->linkedin }}</td>
+                    <td>       
+                        <a href="{{ route('socmed-edit', $item->id) }}" class="btn btn-primary me-md-2" type="button">EDIT</a>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section><!-- End About Section -->
 
 @endsection
